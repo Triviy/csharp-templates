549bae366517c1a78654034c1c0eaa540ce20d8d
Andrii Nadezhdin <andrii.nadezhdin@intellias.com>
2021-11-09 07:57:28 +0200

    move to .net6

---
+++ src/Content/System.API/CompanyName.SystemAPI.TemplateName.API.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CompanyName.SystemAPI.TemplateName.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/Content/System.API/CompanyName.SystemAPI.TemplateName.Facade.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CompanyName.SystemAPI.TemplateName.Facade.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
